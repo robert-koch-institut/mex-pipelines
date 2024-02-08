d764f2fca0ce829c1b87535ca6df1cfca74fcadd
Eric Hesse <hessee@rki.de>
2024-02-08 17:00:09 +0100

    delete deprecated ifsg models

--- mex/ifsg/models/ifsg_variable_group.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-from mex.common.models import BaseModel
-
-
-class IFSGVariableGroup(BaseModel):
-    """Model class for ifsg variable group entities."""
-
-    label: list[dict[str, Any]]
