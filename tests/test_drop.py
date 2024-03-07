b45ce5bf15a975074a7eab65d6ec86fa6bd22a86
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2024-03-07 09:53:43 +0100

    add tests for missing x-system / files on mex-drop

--- tests/test_drop.py
+++ tests/test_drop.py
@@ -2,6 +2,7 @@ from unittest.mock import MagicMock, Mock
 
 import pytest
 from pytest import MonkeyPatch
+from requests.exceptions import HTTPError
 
 from mex.drop import DropApiConnector
 
@@ -11,6 +12,15 @@ def test_list_files() -> None:
     connector = DropApiConnector.get()
     files = connector.list_files("test")
     assert files == ["default"]
+
+
+@pytest.mark.integration
+def test_list_files_of_non_existing_x_system() -> None:
+    connector = DropApiConnector.get()
+    with pytest.raises(
+        HTTPError, match="404.*The requested x-system was not found on this server"
+    ):
+        connector.list_files("does-not-exist")
 
 
 def test_list_files_mocked(monkeypatch: MonkeyPatch) -> None:
@@ -29,6 +39,15 @@ def test_get_file() -> None:
     connector = DropApiConnector.get()
     content = connector.get_file("test", "default")
     assert content == {"foo": "bar", "list": [1, 2, "foo"], "nested": {"foo": "bar"}}
+
+
+@pytest.mark.integration
+def test_get_file_that_does_not_exist() -> None:
+    connector = DropApiConnector.get()
+    with pytest.raises(
+        HTTPError, match="404 .*The requested data was not found on this server"
+    ):
+        connector.get_file("test", "does-not-exist")
 
 
 def test_get_file_mocked(monkeypatch: MonkeyPatch) -> None:
