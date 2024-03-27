239cd63c9748c5169a6b8b6c92326a4722307e9b
erichesse <hessee@rki.de>
2024-03-27 12:40:27 +0100

    Feature/mx 1556 extract grippe web data (#36)
    # PR Context
    <!-- Additional info for the reviewer -->
    
    # Added
    - grippeweb extract
    
    # Changes
    - move MSSQL Server authentication to general settings
    
    # Deprecated
    <!-- Soon-to-be removed features -->
    
    # Removed
    <!-- Definitely removed features -->
    
    # Fixed
    <!-- Fixed bugs -->
    
    # Security
    <!-- Fixed vulnerabilities -->
    
    ---------
    
    Signed-off-by: erichesse <eric.hesse3@yahoo.de>
    Co-authored-by: Kamran Ali <33874616+mr-kamran-ali@users.noreply.github.com>

--- mex/settings.py
+++ mex/settings.py
@@ -39,3 +39,20 @@ class Settings(BaseSettings):
         description="MEx drop API url.",
         validation_alias="MEX_DROP_API_URL",
     )
+
+    kerberos_user: str = Field(
+        "user@domain.tld",
+        description="Kerberos user to authenticate against MSSQL server.",
+    )
+    kerberos_password: SecretStr = Field(
+        SecretStr("password"),
+        description="Kerberos password to authenticate against MSSQL server.",
+    )
+    mssql_connection_dsn: str = Field(
+        "DRIVER={ODBC Driver 18 for SQL Server};SERVER=domain.tld;DATABASE=database",
+        description=(
+            "Connection string for the ODBC Driver for SQL Server: "
+            "https://learn.microsoft.com/en-us/sql/connect/odbc/"
+            "dsn-connection-string-attribute"
+        ),
+    )
