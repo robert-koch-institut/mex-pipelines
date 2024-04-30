2a5ce48944e5ff16fdf14d819fcbcf167ebcb944
esinsj <esinsj@rki.de>
2024-04-30 15:42:38 +0200

    setup new publisher pipeline

---
+++ mex/publisher/main.py
@@ -0,0 +1,15 @@
+from mex.common.cli import entrypoint
+from mex.pipeline import asset, run_job_in_process
+from mex.settings import Settings
+
+@asset(group_name="publisher")
+def publish_merged_items() -> None: 
+    """ Get all merged items from mex-backend and write to ndjson file.
+    """
+    print("Hi world :P")
+
+
+@entrypoint(Settings)
+def run() -> None:
+    """Run the publisher job in-process."""
+    run_job_in_process("publisher")
