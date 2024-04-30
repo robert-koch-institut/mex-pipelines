39ff9bfe30d0c46c304b0301a04b1526b748072c
esinsj <esinsj@rki.de>
2024-04-30 15:50:51 +0200

    fix linting

--- mex/publisher/main.py
+++ mex/publisher/main.py
@@ -1,12 +1,13 @@
 from mex.common.cli import entrypoint
+from mex.common.logging import logger
 from mex.pipeline import asset, run_job_in_process
 from mex.settings import Settings
 
+
 @asset(group_name="publisher")
-def publish_merged_items() -> None: 
-    """ Get all merged items from mex-backend and write to ndjson file.
-    """
-    print("Hi world :P")
+def publish_merged_items() -> None:
+    """Get all merged items from mex-backend and write to ndjson file."""
+    logger.info("Hi world :P")
 
 
 @entrypoint(Settings)
