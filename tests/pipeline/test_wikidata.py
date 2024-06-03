b8df90462fe36d94dc684432af8c8f0e2a3206c5
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2024-06-03 13:45:50 +0200

    expand test

--- tests/pipeline/test_wikidata.py
+++ tests/pipeline/test_wikidata.py
@@ -1,5 +1,8 @@
 from mex.common.models import ExtractedPrimarySource
 from mex.common.wikidata.models.organization import WikidataOrganization
+from mex.common.wikidata.transform import (
+    transform_wikidata_organization_to_extracted_organization,
+)
 from mex.pipeline.wikidata import get_organization_merged_id_by_query
 
 
@@ -7,8 +10,17 @@ def test_get_organization_merged_id_by_query(
     wikidata_organization: WikidataOrganization,
     extracted_primary_sources: dict[str, ExtractedPrimarySource],
 ) -> None:
+    wikidata_primary_source = extracted_primary_sources["wikidata"]
 
     returned = get_organization_merged_id_by_query(
-        {"foo": wikidata_organization}, extracted_primary_sources["wikidata"]
+        {"foo": wikidata_organization}, wikidata_primary_source
     )
-    assert returned == {"foo": 123}
+    assert returned == {}
+
+    extracted_organization = transform_wikidata_organization_to_extracted_organization(
+        wikidata_organization, wikidata_primary_source
+    )
+    returned = get_organization_merged_id_by_query(
+        {"foo": wikidata_organization}, wikidata_primary_source
+    )
+    assert returned == {"foo": extracted_organization.stableTargetId}
