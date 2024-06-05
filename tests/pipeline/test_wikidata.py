ca5b2490cd9b4e8613d128d678da002e84c1a9ba
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2024-06-05 13:46:22 +0200

    add function for combining mapping creation with transform and load

--- tests/pipeline/test_wikidata.py
+++ tests/pipeline/test_wikidata.py
@@ -3,7 +3,10 @@ from mex.common.wikidata.models.organization import WikidataOrganization
 from mex.common.wikidata.transform import (
     transform_wikidata_organization_to_extracted_organization,
 )
-from mex.pipeline.wikidata import get_organization_merged_id_by_query
+from mex.pipeline.wikidata import (
+    get_organization_merged_id_by_query,
+    get_organization_merged_id_by_query_with_transform_and_load,
+)
 
 
 def test_get_organization_merged_id_by_query(
@@ -24,3 +27,15 @@ def test_get_organization_merged_id_by_query(
         {"foo": wikidata_organization}, wikidata_primary_source
     )
     assert returned == {"foo": extracted_organization.stableTargetId}
+
+
+def test_get_organization_merged_id_by_query_with_transform_and_load(
+    wikidata_organization: WikidataOrganization,
+    extracted_primary_sources: dict[str, ExtractedPrimarySource],
+) -> None:
+    wikidata_primary_source = extracted_primary_sources["wikidata"]
+
+    returned = get_organization_merged_id_by_query_with_transform_and_load(
+        {"foo": wikidata_organization}, wikidata_primary_source
+    )
+    assert returned == {"foo": "ga6xh6pgMwgq7DC7r6Wjqg"}
