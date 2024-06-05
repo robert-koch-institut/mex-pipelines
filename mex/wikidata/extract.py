7a91dd25ead0f02fd65cb9dd16740b732d240f29
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2024-06-05 14:11:05 +0200

    remove get_organization_merged_id_by_query

--- mex/wikidata/extract.py
+++ mex/wikidata/extract.py
@@ -1,4 +1,3 @@
-from mex.common.identity import get_provider
 from mex.common.models import ExtractedPrimarySource
 from mex.common.types import MergedOrganizationIdentifier
 from mex.common.wikidata.models.organization import WikidataOrganization
@@ -6,37 +5,6 @@ from mex.sinks import load
 from mex.wikidata.transform import (
     transform_wikidata_organizations_to_extracted_organizations_with_query,
 )
-
-
-def get_organization_merged_id_by_query(
-    wikidata_organizations_by_query: dict[str, WikidataOrganization],
-    wikidata_primary_source: ExtractedPrimarySource,
-) -> dict[str, MergedOrganizationIdentifier]:
-    """Return a mapping from organizations to their stable target ID.
-
-    MergedOrganizationIdentifiers are looked up in the identity provider and will be
-    omitted for any organization that has not yet been assigned an `Identity` there.
-
-    Args:
-        wikidata_organizations_by_query: dict of Extracted organizations by query string
-        wikidata_primary_source: Primary source item for wikidata
-
-    Returns:
-        Dict with organization label keys and stable target ID values
-    """
-    identity_provider = get_provider()
-    organization_stable_target_id_by_query = {}
-    for query, wikidata_organization in wikidata_organizations_by_query.items():
-        identities = identity_provider.fetch(
-            had_primary_source=wikidata_primary_source.stableTargetId,
-            identifier_in_primary_source=wikidata_organization.identifier,
-        )
-        if identities:
-            organization_stable_target_id_by_query[query] = (
-                MergedOrganizationIdentifier(identities[0].stableTargetId)
-            )
-
-    return organization_stable_target_id_by_query
 
 
 def get_organization_merged_id_by_query_with_transform_and_load(
--- tests/wikidata/test_extract.py
+++ tests/wikidata/test_extract.py
@@ -1,33 +1,8 @@
 from mex.common.models import ExtractedPrimarySource
 from mex.common.wikidata.models.organization import WikidataOrganization
-from mex.common.wikidata.transform import (
-    transform_wikidata_organization_to_extracted_organization,
-)
 from mex.wikidata.extract import (
-    get_organization_merged_id_by_query,
     get_organization_merged_id_by_query_with_transform_and_load,
 )
-
-
-def test_get_organization_merged_id_by_query(
-    wikidata_organization: WikidataOrganization,
-    extracted_primary_sources: dict[str, ExtractedPrimarySource],
-) -> None:
-    wikidata_primary_source = extracted_primary_sources["wikidata"]
-
-    returned = get_organization_merged_id_by_query(
-        {"foo": wikidata_organization}, wikidata_primary_source
-    )
-    assert returned == {}
-
-    extracted_organization = transform_wikidata_organization_to_extracted_organization(
-        wikidata_organization, wikidata_primary_source
-    )
-    assert extracted_organization is not None
-    returned = get_organization_merged_id_by_query(
-        {"foo": wikidata_organization}, wikidata_primary_source
-    )
-    assert returned == {"foo": extracted_organization.stableTargetId}
 
 
 def test_get_organization_merged_id_by_query_with_transform_and_load(
