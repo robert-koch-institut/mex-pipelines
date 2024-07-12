bf23717fe70a87816ddfa176fdc26434a4b3e57d
esinsj <72222648+esinsj@users.noreply.github.com>
2024-07-12 13:40:54 +0200

    Update mex/publisher/load.py
    Co-authored-by: Nicolas Drebenstedt <897972+cutoffthetop@users.noreply.github.com>
    Signed-off-by: esinsj <72222648+esinsj@users.noreply.github.com>

--- mex/publisher/load.py
+++ mex/publisher/load.py
@@ -8,6 +8,6 @@ from mex.common.transform import MExEncoder
 
 def write_merged_items(ndjson_path: Path, items: Iterable[dict[str, Any]]) -> None:
     """Write the incoming items into a new-line delimited JSON file."""
-    with open(ndjson_path, "a+", encoding="utf-8") as file:
+    with open(ndjson_path, "w", encoding="utf-8") as file:
         for item in items:
             file.write(json.dumps(item, cls=MExEncoder) + "\n")
