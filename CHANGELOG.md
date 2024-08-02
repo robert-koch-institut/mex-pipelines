f2a3724bce848acd48f7f4582e84fb34f56aa0c1
esinsj <esinsj@rki.de>
2024-08-02 11:37:24 +0200

    initialize convenience function to give back the extractedOrganisation's stableTargetId for a wikidata query

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - transform voxco resources and variables
+- convenience function for wikidata etl pipeline
 
 ### Changes
 
---
+++ mex/wikidata/convenience.py
@@ -0,0 +1,39 @@
+from mex.common.models import ExtractedPrimarySource
+from mex.common.types import MergedOrganizationIdentifier
+from mex.common.wikidata.extract import search_organization_by_label
+from mex.common.wikidata.transform import (
+    transform_wikidata_organization_to_extracted_organization,
+)
+from mex.sinks import load
+
+
+def get_merged_organization_id_by_query_with_extract_transform_and_load(
+    query_string: str,
+    wikidata_primary_source: ExtractedPrimarySource,
+) -> MergedOrganizationIdentifier | None:
+    """Looks up and loads a WikidataOrganization and returns its stableTargetId.
+
+    An organization is searched for in wikidata, transformed into an
+    ExtractedOrganization and loaded into the configured sink.
+    Also it's stable target id is returned.
+
+    Returns:
+        Extracted WikidataOrganization stableTargetId if one matching organization
+        is found in Wikidata lookup.
+        None if multiple matches / no organization is found
+    """
+    found_organization = search_organization_by_label(query_string)
+
+    if found_organization is None:
+        return None
+
+    extracted_organization = transform_wikidata_organization_to_extracted_organization(
+        found_organization, wikidata_primary_source
+    )
+
+    if extracted_organization is None:
+        return None
+
+    load([extracted_organization])
+
+    return extracted_organization.stableTargetId
