3d08fb529f3f982bddedfbd7c6c9b93480b65095
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2024-08-05 11:25:13 +0200

    check content of load

--- tests/wikidata/test_convenience.py
+++ tests/wikidata/test_convenience.py
@@ -6,6 +6,9 @@ from pytest import MonkeyPatch
 from mex.common.models import ExtractedPrimarySource
 from mex.common.types import MergedOrganizationIdentifier
 from mex.common.wikidata.models.organization import WikidataOrganization
+from mex.common.wikidata.transform import (
+    transform_wikidata_organization_to_extracted_organization,
+)
 from mex.wikidata import convenience
 from mex.wikidata.convenience import (
     get_merged_organization_id_by_query_with_extract_transform_and_load,
@@ -23,6 +26,11 @@ def test_get_merged_organization_id_by_query_with_extract_transform_and_load(
 
     query_string = "Robert Koch-Institut"
     wikidata_primary_source = extracted_primary_sources["wikidata"]
+    extracted_wikidata_organization = (
+        transform_wikidata_organization_to_extracted_organization(
+            wikidata_organization, wikidata_primary_source
+        )
+    )
 
     mocked_load = Mock()
     monkeypatch.setattr(convenience, "load", mocked_load)
@@ -30,6 +38,6 @@ def test_get_merged_organization_id_by_query_with_extract_transform_and_load(
     returned = get_merged_organization_id_by_query_with_extract_transform_and_load(
         query_string, wikidata_primary_source
     )
-    mocked_load.assert_called_once()
+    mocked_load.assert_called_once_with([extracted_wikidata_organization])
 
     assert returned == MergedOrganizationIdentifier("ga6xh6pgMwgq7DC7r6Wjqg")
