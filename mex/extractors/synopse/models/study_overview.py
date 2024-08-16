a73c881dce9382b7993b216e491cd069463ce821
erichesse <hessee@rki.de>
2024-08-16 10:37:36 +0200

    Feature/mx 1646 synopse update (#154)
    # PR Context
    
    - [x] issue to resolve: some resource_extended-data-use don't have
    variables(metadata review pending)
    -> MX-1675 created for metadata review
    
    -refactoring of the extractor is out of scope for this ticket,
    suggestions can go into [MX-1667]
    
    Changes:
    -switched from hard coded default values to mapping extractor
    -adapt extractor to old mapping
    -adapt extractor to new mapping
    -adapt extractor to additional mapping changes
    
    
    # Added
    <!-- New features and interfaces -->
    
    # Changes
    - model-update: synopse
    
    # Deprecated
    <!-- Soon-to-be removed features -->
    
    # Removed
    <!-- Definitely removed features -->
    
    # Fixed
    <!-- Fixed bugs -->
    
    # Security
    <!-- Fixed vulnerabilities -->

--- mex/extractors/synopse/models/study_overview.py
+++ mex/extractors/synopse/models/study_overview.py
@@ -9,6 +9,6 @@ class SynopseStudyOverview(BaseModel):
     model_config = ConfigDict(extra="ignore")
 
     studien_id: str = Field(..., alias="StudienID")
-    ds_typ_id: int = Field(..., alias="DStypID")
+    ds_typ_id: int | None = Field(..., alias="DStypID")
     titel_datenset: str = Field(..., alias="Titel_Datenset")
     synopse_id: str = Field(..., alias="SynopseID")
