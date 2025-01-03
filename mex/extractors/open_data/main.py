b865d83013128f49e41111f1292288df7818c8e9
Janina Esins <esinsj@rki.de>
2025-01-03 14:02:31 +0100

    add new extractor shortcut to run job in process

--- mex/extractors/open_data/main.py
+++ mex/extractors/open_data/main.py
@@ -1,3 +1,4 @@
+from mex.common.cli import entrypoint
 from mex.extractors.open_data.extract import (
     extract_parent_resources,
     extract_resource_versions,
@@ -7,6 +8,8 @@ from mex.extractors.open_data.models.source import (
     OpenDataResourceVersion,
 )
 from mex.extractors.pipeline import asset
+from mex.extractors.pipeline.base import run_job_in_process
+from mex.extractors.settings import Settings
 
 
 @asset(group_name="open_data")
@@ -19,3 +22,9 @@ def open_data_parent_resources() -> list[OpenDataParentResource]:
 def open_data_resource_versions() -> list[OpenDataResourceVersion]:
     """Extract all versions of the parent resources from Zenodo."""
     return list(extract_resource_versions())
+
+
+@entrypoint(Settings)
+def run() -> None:
+    """Run the odk extractor job in-process."""
+    run_job_in_process("open_data")
