509904bdadcc9a082dc5547d7bbcf6722cfc38d3
Nicolas Drebenstedt <drebenstedtn@rki.de>
2025-01-28 11:06:17 +0100

    feature/update mex-common to 0.48.0

--- mex/extractors/publisher/main.py
+++ mex/extractors/publisher/main.py
@@ -1,5 +1,6 @@
-from mex.common.backend_api.models import MergedItemsResponse
+from mex.common.backend_api.models import PaginatedItemsContainer
 from mex.common.cli import entrypoint
+from mex.common.models import AnyMergedModel
 from mex.extractors.pipeline import asset, run_job_in_process
 from mex.extractors.publisher.extract import get_merged_items
 from mex.extractors.publisher.filter import filter_merged_items
@@ -8,15 +9,17 @@ from mex.extractors.settings import Settings
 
 
 @asset(group_name="publisher")
-def extract_and_filter_merged_items() -> MergedItemsResponse:
+def extract_and_filter_merged_items() -> PaginatedItemsContainer[AnyMergedModel]:
     """Get merged items from mex-backend and filter them by allow-list."""
     items = get_merged_items()
     filtered = list(filter_merged_items(items))
-    return MergedItemsResponse(items=filtered, total=len(filtered))
+    return PaginatedItemsContainer[AnyMergedModel](items=filtered, total=len(filtered))
 
 
 @asset(group_name="publisher")
-def publish_merged_items(extract_and_filter_merged_items: MergedItemsResponse) -> None:
+def publish_merged_items(
+    extract_and_filter_merged_items: PaginatedItemsContainer[AnyMergedModel],
+) -> None:
     """Write received merged items to ndjson file."""
     write_merged_items(extract_and_filter_merged_items.items)
 
